8d8011eb837b2ad8367b39e04a343002aab2a162
cvernooy23 <cvernooy23@gmail.com>
2016-02-29 18:14:06 -0500

    initial commit

---
+++ monCPU.sh
@@ -0,0 +1,32 @@
+a=$(iostat -c 15 1 | grep -A 1 "%idle" | awk 'NR>1' | awk '{print $6}')
+function float_eval()
+{
+    local stat=0
+    local result=0.0
+    if [[ $# -gt 0 ]]; then
+        result=$(echo "scale=$float_scale; $*" | bc -q 2>/dev/null)
+        stat=$?
+        if [[ $stat -eq 0  &&  -z "$result" ]]; then stat=1; fi
+    fi
+    echo $result
+    return $stat
+}
+function float_cond()
+{
+    local cond=0
+    if [[ $# -gt 0 ]]; then
+        cond=$(echo "$*" | bc -q 2>/dev/null)
+        if [[ -z "$cond" ]]; then cond=0; fi
+        if [[ "$cond" != 0  &&  "$cond" != 1 ]]; then cond=0; fi
+    fi
+    local stat=$((cond == 0))
+    return $stat
+}
+
+if $(float_cond '$a > 5.00'); then
+	echo "warning: $a"
+else
+	echo "ok $a"
+fi
+
+
